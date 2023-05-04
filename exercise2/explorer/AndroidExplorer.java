33e521808884e944558651d4d14c68f6b73a218f
Angelique Anne Valdez <129362498+aneqlique@users.noreply.github.com>
2023-05-04 14:54:37 +0800

    Add files via upload

---
+++ exercise2/explorer/AndroidExplorer.java
@@ -0,0 +1,53 @@
+package Explorers;
+
+import Planets.*;
+
+public class AndroidExplorer implements Explorer {
+    @Override
+    public void visit(Mercury mercury) {
+        System.out.println("san ka punta? to the mercury. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Mars mars) {
+        System.out.println("san ka punta? to the mars. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Earth earth) {
+        System.out.println("san ka punta? to the earth. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Neptune neptune) {
+        System.out.println("san ka punta? to the neptune. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Pluto pluto) {
+        System.out.println("san ka punta? to the pluto. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Saturn saturn) {
+        System.out.println("san ka punta? to the saturn. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Uranus uranus) {
+        System.out.println("san ka punta? to the uranus. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Venus venus) {
+        System.out.println("san ka punta? to the venus. roadtrip. broom broom. skrt! skrt!");
+    }
+
+    @Override
+    public void visit(Jupiter jupiter) {
+        System.out.println("san ka punta? to the jupiter. roadtrip. broom broom. skrt! skrt!");
+    }
+
+
+
+}
